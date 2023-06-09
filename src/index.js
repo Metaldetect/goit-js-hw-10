7813d60c9dc831a04b6a128b0525a7dae5c3b38f
Metaldetect <stas.klymenko@icloud.com>
2023-06-09 15:45:43 +0200

    Update index.js
    Update Slimselect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import '/node_modules/slim-select/dist/slimselect.css';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfoContainer = document.querySelector('.cat-info');
@@ -13,6 +15,9 @@ function populateBreedsSelect(breeds) {
       `<option value="${value}">${label}</option>`
     );
   });
+  new SlimSelect({
+    select: breedSelect,
+  });
 }
 
 function displayCatInfo(cat) {
@@ -20,32 +25,26 @@ function displayCatInfo(cat) {
     cat.breeds[0];
 
   catInfoContainer.innerHTML = `
-  <img src="${cat.url}" alt="Cat Image" width='1080' class="cat-image">
-  <div class="cat-container-info">
-    <h2 class="cat-name">Name: ${name}</h2>
-    <h2 class="country">From: ${origin}</h2>
-    <p class="cat-description">Description: ${description}</p>
-    <p class="cat-temperament">Temperament: ${temperament}</p>
-    <a href="${wikipedia_url}" class="wikipedia-link" target="_blank">Wikipedia</a>
-  </div>
-`;
+    <img src="${cat.url}" alt="Cat Image" width="1080" class="cat-image">
+    <div class="cat-container-info">
+      <h2 class="cat-name">Name: ${name}</h2>
+      <h2 class="country">From: ${origin}</h2>
+      <p class="cat-description">Description: ${description}</p>
+      <p class="cat-temperament">Temperament: ${temperament}</p>
+      <a href="${wikipedia_url}" class="wikipedia-link" target="_blank">Wikipedia</a>
+    </div>
+  `;
 
-  const catImage = catInfoContainer.querySelector('.cat-image');
-
-  catImage.addEventListener('load', () => {
-    loaderElement.classList.add('hidden');
-    catInfoContainer.classList.remove('hidden');
-  });
-
-  catImage.addEventListener('error', () => {
-    loaderElement.classList.add('hidden');
-    handleError('Failed to load image');
-  });
+  loaderElement.classList.add('hidden');
+  catInfoContainer.classList.remove('hidden');
 }
 
 function handleError(error) {
   loaderElement.classList.add('hidden');
   console.error('Oops! Something went wrong:', error);
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!'
+  );
 }
 
 function handleBreedSelectChange() {
@@ -63,6 +62,11 @@ function handleBreedSelectChange() {
   fetchCatByBreed(selectedBreedId).then(displayCatInfo).catch(handleError);
 }
 
-fetchBreeds().then(populateBreedsSelect).catch(handleError);
+fetchBreeds()
+  .then(data => {
+    populateBreedsSelect(data);
+    loaderElement.classList.add('hidden');
+  })
+  .catch(handleError);
 
 breedSelect.addEventListener('change', handleBreedSelectChange);
