196b661dfaa2538a542a19b270379308e761d86b
Metaldetect <stas.klymenko@icloud.com>
2023-06-09 14:49:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,58 +1,68 @@
 import './css/styles.css';
-// import Notiflix from 'notiflix';
-// import debounce from 'lodash.debounce';
+import SlimSelect from 'slim-select';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector('.breed-select');
-const catContainer = document.querySelector('.cat-info');
+const catInfoContainer = document.querySelector('.cat-info');
+const loaderElement = document.querySelector('.loader');
 
-const API_KEY =
-  'live_KOSlRsnZ1Bv6BJRA6bXEetoG5xvkYoNeTTQNNmhZyLCwvAB8FjNupiBSj7SvH844';
+function populateBreedsSelect(breeds) {
+  breeds.forEach(({ value, label }) => {
+    breedSelect.insertAdjacentHTML(
+      'beforeend',
+      `<option value="${value}">${label}</option>`
+    );
+  });
+}
 
-fetchBreeds(API_KEY)
-  .then(breeds => {
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.value;
-      option.textContent = breed.label;
-      breedSelect.appendChild(option);
-    });
-  })
-  .catch(error => {
-    console.error('Сталася помилка:', error);
+function displayCatInfo(cat) {
+  const { name, origin, description, temperament, wikipedia_url } =
+    cat.breeds[0];
+
+  catInfoContainer.innerHTML = `
+  <img src="${cat.url}" alt="Cat Image" width='1080' class="cat-image">
+  <div class="cat-container-info">
+    <h2 class="cat-name">Name: ${name}</h2>
+    <h2 class="country">From: ${origin}</h2>
+    <p class="cat-description">Description: ${description}</p>
+    <p class="cat-temperament">Temperament: ${temperament}</p>
+    <a href="${wikipedia_url}" class="wikipedia-link" target="_blank">Wikipedia</a>
+  </div>
+`;
+
+  const catImage = catInfoContainer.querySelector('.cat-image');
+
+  catImage.addEventListener('load', () => {
+    loaderElement.classList.add('hidden');
+    catInfoContainer.classList.remove('hidden');
   });
 
-breedSelect.addEventListener('change', () => {
+  catImage.addEventListener('error', () => {
+    loaderElement.classList.add('hidden');
+    handleError('Failed to load image');
+  });
+}
+
+function handleError(error) {
+  loaderElement.classList.add('hidden');
+  console.error('Oops! Something went wrong:', error);
+}
+
+function handleBreedSelectChange() {
   const selectedBreedId = breedSelect.value;
 
-  fetchCatByBreed(selectedBreedId)
-    .then(cat => {
-      catContainer.innerHTML = '';
+  if (!selectedBreedId) {
+    catInfoContainer.innerHTML = '';
+    catInfoContainer.classList.add('hidden');
+    return;
+  }
 
-      const catImageElement = document.createElement('img');
-      catImageElement.src = cat.url;
-      catImageElement.alt = 'Cat Image';
-      catImageElement.width = 480;
-      catContainer.appendChild(catImageElement);
+  loaderElement.classList.remove('hidden');
+  catInfoContainer.classList.add('hidden');
 
-      const breedNameElement = document.createElement('h2');
-      breedNameElement.textContent = cat.breeds[0].name;
-      catContainer.appendChild(breedNameElement);
+  fetchCatByBreed(selectedBreedId).then(displayCatInfo).catch(handleError);
+}
 
-      const breedDescriptionElement = document.createElement('p');
-      breedDescriptionElement.textContent = `Description: ${cat.breeds[0].description}`;
-      catContainer.appendChild(breedDescriptionElement);
+fetchBreeds().then(populateBreedsSelect).catch(handleError);
 
-      const breedTemperamentElement = document.createElement('p');
-      breedTemperamentElement.textContent = `Temperament: ${cat.breeds[0].temperament}`;
-      catContainer.appendChild(breedTemperamentElement);
-
-      const breedWikipediaElement = document.createElement('a');
-      breedWikipediaElement.href = cat.breeds[0].wikipedia_url;
-      breedWikipediaElement.textContent = 'Wikipedia';
-      catContainer.appendChild(breedWikipediaElement);
-    })
-    .catch(error => {
-      console.error('Сталася помилка при отриманні даних про кота:', error);
-    });
-});
+breedSelect.addEventListener('change', handleBreedSelectChange);
